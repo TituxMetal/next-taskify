fd383e15c5c02e8f861a3d90f3e8980bf925f420
Titux Metal (DEV) <github@lgdweb.fr>
2023-12-21 22:30:55 +0100

    Add Clerk signin and signup pages Add layout for clerk auth pages Add middleware, add home page in public routes

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,5 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className='h-full flex items-center justify-center bg-neutral-200'>{children}</div>
+}
+
+export default ClerkLayout
